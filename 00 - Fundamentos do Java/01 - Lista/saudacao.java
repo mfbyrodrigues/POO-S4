7ce6b57b13858d87af16c75bca64d703ed527df4
M. Fernanda <n.nandcchi@gmail.com>
2025-08-08 11:38:19 -0300

    Add files via upload

---
+++ 00 - Fundamentos do Java/01 - Lista/saudacao.java
@@ -0,0 +1,16 @@
+//4. Desenvolva um programa que solicite ao usuário que digite seu nome e, em seguida, imprima uma mensagem de saudação personalizada, como "Bem-vindo, [nome do usuário]!".
+
+import java.util.Scanner;
+
+public class saudacao {
+    
+    public static void main(String[] args) {
+        
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Digite seu nome: ");
+        String nome = scanner.nextLine();
+
+        System.out.println("Bom dia, " + nome + "!");
+    }
+}
